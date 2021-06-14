5848f95b70eded0bfb189930a8749e288e26f2bc
Brian Nguyen <BrianCTNguyen@knights.ucf.edu>
2021-06-13 21:04:58 -0400

    Assignment 2

--- src/main/java/oop/assignment2/ex25/base/Solution25.java
+++ src/main/java/oop/assignment2/ex25/base/Solution25.java
@@ -7,10 +7,55 @@ import java.util.Scanner;
  *  Copyright 2021 Brian Nguyen
  */
 
-public class Solution25 {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
+import java.util.Scanner;
 
+public class Solution25 {
+    private static final Scanner in = new Scanner(System.in);
+
+    private static String password;
+
+    public static void main(String[] args) {
+        readUserInput();
+
+        String output = passwordValidator(password);
+
+        System.out.println(output);
     }
 
+    public static void readUserInput(){
+        password = in.next();
+    }
+
+    public static String passwordValidator(String password) {
+
+        int digits = 0;
+        int symbols = 0;
+        int letters = 0;
+
+        String result = "The password '" + password + "' is a ";
+
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) {
+                digits++;
+            } else if (Character.isLetter(password.charAt(i))) {
+                letters++;
+            } else {
+                symbols++;
+            }
+        }
+        if (password.length() < 8) {
+            if (digits == password.length()) {
+                result += "very weak password";
+            } else if (letters == password.length()) {
+                result += "weak password";
+            }
+        } else {
+            if (letters != 0 && digits != 0 && symbols != 0) {
+                result += "very strong password";
+            } else if (letters != 0 && digits != 0) {
+                result += "strong password";
+            }
+        }
+        return result;
+    }
 }
